98af1d75c5542d1acef2448cc963ebe305e3d67f
pedrogriff <marcincowski@gmail.com>
2020-02-07 17:06:27 -0300

    Movendo arquivos para pasta da aula

--- Aula 0702/functions.js
+++ Aula 0702/functions.js
@@ -1,44 +1,25 @@
+let pets = require("./pets.json")
+//console.log(pets)
 
-  // Carregando o json do arquivo pets e armazenando
-  // na variável pets como um array;
-  let pets = require("./pets.json");
+const servDarBanho = (pet) => {
+    console.log("Dando banho em " + pet.nome)
+    pet.servicos.push("Banho")
+    return pet
+}
 
-  //Transformando strings de data em objetos Date
-  let objetosDate = pets.map(
-    (pet) => {
-        pet.nascimento = new Date(pet.nascimento)
-        return pet
-    }
-  )
+const servTosar = (pet) => {
+    console.log("Dando banho em " + pet.nome)
+    pet.servicos.push("Tosa")
+    return pet
+}
 
-  const servDarBanho = pet => {
-    console.log("Dando banho em " + pet.nome);
-    pet.servicos.push("banho");
-    return pet;
-  };
-  
-  // Criem uma função que faz o equivalente
-  // a servDarBanho, mas que TOSA
-  const servTosar = pet => {
-    console.log("Tosando " + pet.nome);
-    pet.servicos.push("tosa");
-    return pet;
-  };
-  
-  // Verifica se o cachorro foi vacinado
-  // Caso não tenha sido, altera a prop vacinado para
-  // true e adiciona "vacina" ao array de servicos
-  // Caso ja tenha sido vacinado, imprime uma msg de erro
-  const servVacinar = pet => {
-    if (pet.vacinado) {
-      console.log(pet.nome + " já foi vacinado");
+const servCastrar = (pet) => {
+    if (pet.castrado) {
+        console.log(pet.nome + " já é castrado.")
     } else {
-      pet.vacinado = true;
-      pet.servicos.push("vacina");
-      console.log(pet.nome + " vacinado");
+        pet.servicos.push("Castrar")
     }
-    return pet;
-  };
+ return pet
+}
 
-console.log(pets.map(servVacinar))
- // console.log(pets)
+console.log(servCastrar(pets[1]))
